e0a4b8fc0d4bda4330601ae32670d991858886a0
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-30 12:19:23 +0200

    add cicle for stam name and surname of students

--- main.js
+++ main.js
@@ -3,9 +3,41 @@ let student = {
     surname: "Pasqualino",
     age: 24,
     peso: 78.5
-};
+}
 
 
 for (var key in student) {
     console.log(key + ": " + student[key]);
   }
+
+
+
+
+
+  let students = [
+    {
+      name: "Mario",
+      surname: "Rossi",
+      età: 18,
+      peso: 70
+    },
+    {
+      name: "Luca",
+      surname: "Verdi",
+      età: 20,
+      peso: 65
+    },
+    {
+      name: "Giulia",
+      surname: "Bianchi",
+      età: 19,
+      peso: 60
+    }
+  ];
+  
+  // Ciclo per stampare nome e cognome di ogni studente
+  for (let i = 0; i < students.length; i++) {
+    console.log("Nome: " + students[i].name + ", Cognome: " + students[i].surname);
+  } 
+
+  l
